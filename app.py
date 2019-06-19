081ce947630377c2e226740a897e73ca108a53df
Vladimir Kurbatov <warkb@yandex.ru>
2019-06-19 20:45:29 +0400

    сделал получение id пользователя

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_handlers=True)
 
 from views import index
 
--- websockets.py
+++ websockets.py
@@ -1,18 +1,22 @@
 from flask_socketio import send, emit
 from app import socketio
 from time import sleep
+from flask import request
 
 counter = 0
 
-@socketio.on('client_connected')
-def handle_client_connect_event(json):
-    print('received json: {0}'.format(str(json)))
+players = {}
+
+@socketio.on('connect')
+def handle_client_connect_event():
+    playerid = 1
+    print('eee')
+    emit('hi', f'playerId = {request.sid}')
 
 @socketio.on('message')
 def handle_json_button(json, smf):
     # it will forward the json to all clients.
     send(json, json=True)
-
 
 @socketio.on('alert_button')
 def handle_alert_event(json):
